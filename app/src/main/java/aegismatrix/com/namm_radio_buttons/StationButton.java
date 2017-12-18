73bf461fa5773308606b3eab525d07cf72ef9327
Dhiraj <dhiraj.na@gmail.com>
2017-12-18 06:16:16 +0530

    Station Button

--- app/src/main/java/aegismatrix/com/namm_radio_buttons/StationButton.java
+++ app/src/main/java/aegismatrix/com/namm_radio_buttons/StationButton.java
@@ -1,8 +1,11 @@
 package aegismatrix.com.namm_radio_buttons;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.CornerPathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
@@ -11,35 +14,49 @@ import android.graphics.drawable.shapes.PathShape;
 import android.util.AttributeSet;
 import android.widget.Button;
 
+import aegismatrix.com.namm_radio.R;
+
 /**
  * Created by Dhiraj on 20-10-2017.
  */
 
 public class StationButton extends Button {
-    private Paint mFillPaint = new Paint(), mStrokePaint = new Paint();
+    private Paint mFillPaint = new Paint();
     private RectF mRectF = new RectF();
-    private Path path = new Path();
+    private Path mTrapazoidPath = new Path(), mImagePath = new Path();
     private ShapeDrawable mTrapezoid;
+    private Bitmap tempBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.india);
     private static float D_ZERO = 0.0f;
     private static float D_HUNDRED = 100.0f;
     private static float D_TWO_HUNDRED = 200.0f;
+    private static float D_ONE_TWENTY = 120.0f;
 
     public StationButton(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mFillPaint.setColor(Color.parseColor("#90343746"));
+        mFillPaint.setColor(Color.GREEN);
         mFillPaint.setStyle(Paint.Style.FILL);
-        float radius = getHeight() / 2;
-        mRectF.set(getLeft(), getTop(), getLeft() + radius, getBottom() + radius);
-        path.moveTo(D_ZERO, D_ZERO);
-        path.lineTo(D_HUNDRED, D_ZERO);
-        path.lineTo(D_TWO_HUNDRED, D_HUNDRED);
-        path.lineTo(D_ZERO, D_HUNDRED);
-        path.lineTo(D_ZERO, D_ZERO);
-
-        mTrapezoid = new ShapeDrawable(new PathShape(path, 200.0f, 100.0f));
+        mFillPaint.setStrokeWidth(1.0f);
+        mFillPaint.setShadowLayer(10.0f, 10.0f, 10.0f, Color.parseColor("#7f000000"));
+        CornerPathEffect effect = new CornerPathEffect(12.0f);
+        mFillPaint.setPathEffect(effect);
+        mTrapazoidPath.moveTo(D_ZERO, D_ZERO);
+        mTrapazoidPath.lineTo(D_ONE_TWENTY, D_ZERO);
+        mTrapazoidPath.lineTo(D_TWO_HUNDRED, D_HUNDRED);
+        mTrapazoidPath.lineTo(D_ZERO, D_HUNDRED);
+        mTrapazoidPath.lineTo(D_ZERO, D_ZERO);
+        mTrapazoidPath.close();
+        mTrapezoid = new ShapeDrawable(new PathShape(mTrapazoidPath, D_TWO_HUNDRED, D_HUNDRED));
         mTrapezoid.getPaint().setStyle(Paint.Style.FILL);
         mTrapezoid.getPaint().setStrokeWidth(1.0f);
-        mTrapezoid.getPaint().setColor(Color.GREEN);
+        mTrapezoid.getPaint().setPathEffect(effect);
+        mTrapezoid.getPaint().setColor(Color.parseColor("#90343746"));
+        mImagePath = new Path();
+        mImagePath.setFillType(Path.FillType.EVEN_ODD);
+        mImagePath.moveTo(D_TWO_HUNDRED + 71.0f, D_ZERO);
+        mImagePath.lineTo(getWidth(), D_ZERO);
+        mImagePath.lineTo(getWidth(), getHeight());
+        mImagePath.lineTo(D_TWO_HUNDRED + 71.0f, D_ZERO);
+        mImagePath.close();
     }
 
     @Override
@@ -49,26 +66,12 @@ public class StationButton extends Button {
 
     @Override
     protected void onDraw(Canvas canvas) {
-
-        mFillPaint.setColor(Color.parseColor("#90343746"));
-        mFillPaint.setStyle(Paint.Style.FILL);
-        float radius = getHeight() / 2;
-
-        // canvas.drawArc(mRectF, 90, 180, false, mFillPaint);
-        // canvas.drawRect(getLeft() + radius, getTop(), getRight() - radius - 50, getBottom() - 23, mFillPaint);
-//        mStrokePaint.setColor(Color.BLACK);
-//        mStrokePaint.setStyle(Paint.Style.STROKE);
-//        mStrokePaint.setStrokeWidth(2);
-//        mStrokePaint.setStrokeCap(Paint.Cap.ROUND);
-//        canvas.drawArc(mRectF, 90, 180, false, mStrokePaint);
-//        path.arcTo();
-//        path.moveTo(getLeft() + radius, getLeft() + radius);
-//        path.lineTo(100.0f, 0.0f);
-//        path.lineTo(200.0f, 100.0f);
-//        path.lineTo(0.0f, 100.0f);
-//        path.lineTo(0.0f, 0.0f);
-//        mRectF.set(200, 400, 450, 600);
-//        canvas.drawRoundRect(mRectF, 50, 100, mFillPaint);
         mTrapezoid.draw(canvas);
+        canvas.drawBitmap(tempBitmap, tempBitmap.getWidth(), tempBitmap.getHeight(), mFillPaint);
+        canvas.drawPath(mImagePath, mFillPaint);
+        canvas.save();
+        canvas.restore();
+        invalidate();
     }
+
 }
